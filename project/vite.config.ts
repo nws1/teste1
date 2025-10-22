6a469004328b78505183a7a41a90ce3846cc866a
Nicolas W. Schmidt <52413378+nws1@users.noreply.github.com>
2025-10-22 00:09:55 +0000

    body add script

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // garante paths relativos no deploy
+  base: './', // caminhos relativos
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
 })
